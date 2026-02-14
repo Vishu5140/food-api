df436c59b01e1f99ced227594ec4e9e36e9c1c76
Vishu5140 <onlymartyr5140@gmail.com>
2026-02-14 13:33:58 +0530

    test minimal vercel api

--- api/food.js
+++ api/food.js
@@ -1,17 +1,3 @@
-import fs from "fs";
-import path from "path";
-
 export default function handler(req, res) {
-  try {
-    const filePath = path.join(process.cwd(), "food.json");
-    const jsonData = fs.readFileSync(filePath, "utf-8");
-    const data = JSON.parse(jsonData);
-
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({
-      error: "Failed to load food data",
-      details: error.message,
-    });
-  }
+  res.status(200).json({ message: "API IS WORKING" });
 }
